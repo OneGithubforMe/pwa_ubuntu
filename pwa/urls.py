7242acdb0a37d19f3962f4e3def37e4e3fc72148
ghani-tsaw <ghanshyam.tsaw@gmail.com>
2021-04-23 14:46:55 +0530

    initial commit

---
+++ pwa/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('send_push', send_push),
+    path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/x-javascript')),
+    path('webpush/', include('webpush.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
